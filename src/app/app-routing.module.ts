7a50f642ab7d12d159fc0a1c07c42a3bc755e805
Bernardo Fusco <blfuscomail@gmail.com>
2023-09-05 13:11:33 -0300

    Commit dev-angular (commit on 'Master')

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NovProdComponent } from './nov-prod/nov-prod.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './login.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'produto', component: ProdutoComponent, canActivate: [LoginService] },
+  {
+    path: 'novoprod',
+    component: NovProdComponent,
+    canActivate: [LoginService],
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LateralComponent } from './lateral/lateral.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { NovProdComponent } from './nov-prod/nov-prod.component';
+import { ProdutoService } from './produto.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { LoginService } from './login.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProdutoServidorService } from './produto-servidor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    LateralComponent,
+    HomeComponent,
+    ProdutoComponent,
+    NovProdComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(ProdutoServidorService, {
+      dataEncapsulation: false,
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ProdutoService, LoginService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
